ee3edc4f611b399142e3d3e2b5474c3b75a36588
jamesarry <emmanuel.okoh70@yahoo.com>
2020-01-26 08:54:05 +0000

    Implemented stripe payment system and change title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -34,8 +35,14 @@ const CheckoutPage = ({cartItems, total}) => (
     
 
       <div className="total">
-        <span>Total: ${total}</span>
+        <div>Total: ${total}</div>
+        <div className="test-warning">
+           *Please use the following test credit card for payment
+           <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
       </div>
+      <StripeCheckoutButton  price={total} />
     </div>
 );
 
